7db2c44fbb95aaabc62bea4a640bd1e159f9ff8d
Abhishek <narayan.abhishek.28@gmailcom>
2021-01-24 10:29:21 +0530

    Simon game new push

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
+/*jshint esversion: 6 */
+
 var colorSet = ["red", "green", "blue", "yellow"];
-
 var userClickedColors = [];
-
 var colorSequence = [];
-
 var counter = 0;
-
 var flag = false;
-
 $(".center-btn").click(function() {
   if (!flag) {
     $(".level-title").text("Level " + counter);
@@ -15,60 +12,61 @@ $(".center-btn").click(function() {
     flag = true;
   }
 });
-
-
 $(".box").click(function() {
   var userChosenColor = $(this).attr("class").split(" ")[0];
   userClickedColors.push(userChosenColor);
-  flickerColor(userChosenColor);
   playButtonSound(userChosenColor);
   checkAnswer(userClickedColors.length-1);
 });
-
-
 function checkAnswer(currentLevel){
   if (colorSequence[currentLevel] === userClickedColors[currentLevel]) {
     if (colorSequence.length === userClickedColors.length){
-      setTimeout(function (){
-        nextSequence()
-      },1000);
+        nextSequence();
+      userClickedColors=[];
     }
   } else {
     (new Audio("sounds/wrong.mp3")).play();
     $("body").addClass("game-over");
     setTimeout(function(){
-      $("body").removeClass("game-over")},200);
-    $(".level-title").text("Game Over, Press center button to Restart");
+      $("body").removeClass("game-over");},200);
+    $(".level-title").text("You have conquered "+(counter-1)+" levels, press center button to restart");
+    $(".score").css("visibility","visible");
+    $(".score").append(" "+counter-1);
+    $(".center-btn").click(function() {
+      location.reload();
+    });
   }
 }
-
 function startOver(){
   counter=0;
   colorSequence = [];
   userClickedColors = [];
-  flag = false;
+  $(".level-title").text("Level " + counter);
+  nextSequence();
+  flag = true;
 }
-
 function playButtonSound(button){
   (new Audio("sounds/"+button+".mp3")).play();
 }
-
 function nextSequence() {
   counter++;
   $(".level-title").text("Level " + counter);
-  var randomNum = randNum = Math.floor(Math.random() * 4);
+  var randomNum = Math.floor(Math.random() * 4);
   var chosenColor = colorSet[randomNum];
   colorSequence.push(chosenColor);
-  for(i in colorSequence){
-    console.log("random color selected "+colorSet[i]);
-  }
   flickerColor(colorSequence);
 }
-
 function flickerColor(arrayColor) {
-  for (var i = 0; i < arrayColor.length; i++) {
-    setTimeout(function (){
-      $(".box").filter("." + arrayColor[i]).fadeOut(250).fadeIn(250);
-    },1000);
-  }
+  arrayColor.forEach((color,index)=>{
+    setTimeout(()=>{
+      flicker(color);
+    },(index+1)*600);
+  });
+  // $(".box").filter("." + arrayColor[i]).fadeOut(500).fadeIn(500);
+}
+function flicker(item){
+  $(".box").filter("." + item).addClass("hidden");
+  setTimeout(function(){
+      $(".box").filter("." +item).removeClass("hidden");
+  },500);
 }
